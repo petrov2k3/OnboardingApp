dbf8001c2a5da5bdd1da644ce665b95c80ff87ee
Ivan Petrov <ipetrov2k3@gmail.com>
2025-11-03 22:16:39 +0200

    initial UIKit project

---
+++ OnboardingApp/OnboardingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnboardingApp
+//
+//  Created by Ivan Petrov on 03.11.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
